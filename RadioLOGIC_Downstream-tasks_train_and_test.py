efa41865cffaf3f6bd5acd37c4699ad5fe28803d
TZ <t.zhang@nki.nl>
2023-01-23 14:56:13 +0100

    Create RadioLOGIC_Downstream-tasks_train_and_test.py

---
+++ RadioLOGIC_Downstream-tasks_train_and_test.py
@@ -0,0 +1,3 @@
+### Downstream tasks include predicting BI-RADS scores and predicting pathological outcome. 
+### We make models through transfer learning to complete prediction tasks. So please refer to "RadioLOGIC_Repomics_train_and_test.py" for the code.
+### Please contact us if you have any questions.
